a39d715ca19629785f235a23c6531f011e13aa2a
bagashezky <bagashpratama95@gmail.com>
2023-03-20 15:50:11 +0700

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,32 @@
+import { Container } from '@mui/system'
 import React from 'react'
 import Navbar from './Navbar'
+import {SearchIcon} from '@mui/icons-material/Search';
+// import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import List from '@mui/material/List';
 
 export default function Home() {
   return (
     <div className='home-container'>
-      <Navbar/>
+      <Navbar />
+      <div className='intro'>
+        <Container className="text-white text-center d-flex justify-content-center align-items-center">
+          <h1 className='tx-main1'>Temukan data Ketahanan Pangan di Jawa Barat dengan cepat, tepat, dan akurat</h1>
+          <h2 className='tx-main2'>Mulai dari stok, harga, hingga distribusi semuanya ada disini!
+            Dari data beras, bawang merah, hingga daging sapi  bisa kamu temukan hanya dengan satu kali klik!</h2>
+          <div className="SearchIcon">
+          <i class="SearchIcon"></i>
+                <TextField
+                  id="outlined-basic"
+                  variant="outlined"
+                  fullWidth
+                  label="Search"
+                />
+              <List />
+          </div>
+        </Container>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,34 +25,12 @@ export default function Navbar() {
         <img src={logosimawas} alt="" />
       </div>
       <div className="navbar-links-container">
-        <a href="">Beranda</a>
-        <a href="">Dashboard</a>
-        <a href="">Dataset</a>
-        <a href="">Ketahanan Pangan</a>
-        <a href="">Peternakan</a>
+        <a href="#">Beranda</a>
+        <a href="#">Dashboard</a>
+        <a href="#">Dataset</a>
+        <a href="#">Ketahanan Pangan</a>
+        <a href="#">Peternakan</a>
       </div>
-      {/* <div className="navbar-menu-container">
-        <HiOutlineBars3 onclick={() => setOpenMenu(true)} />
-      </div>
-      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={() => setOpenMenu(false)}
-          onKeyDown={() => setOpenMenu(false)}
-        >
-           <List>
-            {menuOptions.map((item) => (
-              <ListItem key={item.text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText primary={item.text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Drawer> */}
     </nav>
   );
 }
